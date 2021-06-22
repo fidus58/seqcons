1f4399301685da81d76a86e1d0cfb107fc1f4c79
fidus58 <ahrens@informatik.hu-berlin.de>
2021-06-22 21:07:54 +0200

    Initial Commit

---
+++ seqcons/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  seqcons
+//
+//  Created by Klaus Ahrens on 22.06.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
