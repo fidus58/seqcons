9001a4eb79d1fc1d64ca4599ee844bb43ad06c34
fidus58 <ahrens@informatik.hu-berlin.de>
2021-06-22 21:48:45 +0200

    racy

--- seqcons/main.cpp
+++ seqcons/main.cpp
@@ -6,9 +6,32 @@
 //
 
 #include <iostream>
+#include <thread>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int x,y;
+int r1,r2;
+
+void t1f(){
+    x = 1;
+    r1 = y;
+}
+
+void t2f(){
+    y = 1;
+    r2 = x;
+}
+
+int main() {
+    while(true) {
+        std::cout<<".";
+        x=y=r1=r2=0;
+        std::thread t1{t1f};
+        std::thread t2{t2f};
+        t1.join();
+        t2.join();
+        if(!r1 && !r2) {
+            std::cout<<r1<<r2<<"\n";
+            return 1;
+        }
+    }
 }
